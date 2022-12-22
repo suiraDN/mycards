8b07ee34f5ebac93d8cb61ccca585ba6c2d0fb26
Darius Navickas <dariusnaw@gmail.com>
2022-12-22 22:54:12 +0200

    added modal on cards

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-// function togglePicture(div) {
-// 	var img = div.getElementsByTagName('img')[0];
-// 	if (img.style.display === 'none') {
-// 		img.style.display = 'block';
-// 	} else {
-// 		img.style.display = 'none';
-// 	}
-// }
+const buttons = document.querySelectorAll('.loycard');
+const modal = document.getElementById('modal');
+const modalImage = document.getElementById('modal-image');
+const closeButton = document.getElementById('close-button');
 
-function togglePicture(div) {
-	let img = div.getElementsByTagName('img')[0];
-	if (img.classList.contains('visible')) {
-		img.classList.remove('visible');
-	} else {
-		img.classList.add('visible');
+buttons.forEach((button) => {
+	button.addEventListener('click', () => {
+		// Get the image src from the button's data attribute
+		const imageSrc = button.getAttribute('data-image');
+		modalImage.setAttribute('src', imageSrc);
+
+		// Show the modal
+		modal.style.display = 'block';
+	});
+});
+
+closeButton.addEventListener('click', () => {
+	// Hide the modal
+	modal.style.display = 'none';
+});
+
+modal.addEventListener('click', (event) => {
+	// Only hide the modal if the click was not on the modal content
+	if (event.target !== modalContent) {
+		modal.style.display = 'none';
 	}
-}
+});
